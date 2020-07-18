b5144af4c169d9fa9dbdf667d6e65b2a3b02a809
79 <56512657+791107689@users.noreply.github.com>
2020-07-18 21:48:37 +0800

    Update edit_username.py

--- edit_username.py
+++ edit_username.py
@@ -16,14 +16,11 @@ def edit_username(new_user_name, ID, loc = ''):
             json.dump({}, file_object)
     if str(ID) in user_list.keys():
         if new_user_name == user_list[str(ID)]:
-            print(msg_fail)
             return msg_fail
         else:
             user_list[str(ID)] = new_user_name
             with open(filename, 'w') as file_object:
                 json.dump(user_list, file_object)
-            print(msg_success)
             return msg_success
     else:
-        print(msg_unregiter)
         return msg_unregiter
