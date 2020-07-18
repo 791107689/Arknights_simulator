ffa42e3b3f9a2e9c6da02ba11051b4a2a8de55ba
79 <56512657+791107689@users.noreply.github.com>
2020-07-18 22:09:31 +0800

    Update Arknights.py

--- Arknights.py
+++ Arknights.py
@@ -1,7 +1,7 @@
 import time
-
 from look_for import look_for
 from register import register
+from item import item_add
 
 loc = ''
 #loc = 'C:\\BaiduNetdiskDownload\\方舟模拟器\\Arknights\\Arknights\\'
@@ -11,3 +11,4 @@ register('admin','admin_id')
 while(1):
     time.sleep(1)
     print(look_for('admin_id'))
+    print(item_add('admin_id' ,'测试物品'))
