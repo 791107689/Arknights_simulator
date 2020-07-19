171c5248032140952132e56e1cf82639b6960d82
79 <56512657+791107689@users.noreply.github.com>
2020-07-19 22:59:20 +0800

    Update Arknights.py

--- Arknights.py
+++ Arknights.py
@@ -1,15 +1,16 @@
 import time
-from look_for import look_for
+from look_for import look_for_ten
 from register import register
 from item import item_add
 from item import item_del
 from item import item_check
 
+loc = ''
 
 register('admin','admin_id')
 while(1):
     time.sleep(1)
-    print(look_for('admin_id'))
+    print(look_for_ten('admin_id'))
     print(item_add('admin_id' ,'测试物品'))
     print(item_del('admin_id' ,'测试物品'))
     print(item_check('admin_id'))
