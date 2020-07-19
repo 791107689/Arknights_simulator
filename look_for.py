b786aef62ff8eccb6af7f5e0c39e5b920a928ea3
79 <56512657+791107689@users.noreply.github.com>
2020-07-19 22:58:20 +0800

    Update look_for.py

--- look_for.py
+++ look_for.py
@@ -1,8 +1,9 @@
+from PIL import Image
 import random
 import json
 
-#look_for(QQ号,素材存放位置)
-#寻访干员 未注册用户运行时返回str（提示注册） 已注册用户返回dic（储存干员信息）
+#look_for(QQ,素材位置) 寻访 返回值为储存干员信息的dict
+#look_for_ten(QQ,素材位置) 十连 返回值为list（储存十连结果） 并生成十连结果图像（但图像存在问题）
 
 def look_for(ID,loc = ''):
 
@@ -62,7 +63,8 @@ def look_for(ID,loc = ''):
         else:
             card_list.append(card)
             card = ''
-    print(card_list)
+    #print(card_list)
+
     filename = loc+ '用户数据\\' + ID + '.json'
     with open(filename, 'w') as file_object:
         json.dump(user_date, file_object)
@@ -70,3 +72,26 @@ def look_for(ID,loc = ''):
     inf['干员'] = random.choice(card_list)
 
     return inf
+
+def look_for_ten(ID,loc = ''):
+    result = []
+
+    for i in range(10):
+        result.append(look_for(ID,loc)['干员'])
+
+
+    size_x = 151
+    size_y = 443
+    box = (371, 0, 371 + size_x ,0 + size_y)
+    target = Image.new('RGB', (size_x*10 , size_y))
+
+    for i in range(10):
+        pic = loc + '立绘\\' + str(result[i]) + '.png'
+        img = Image.open(pic)
+        region = img.crop(box)
+        target.paste(region,(i*size_x,0,i*size_x + size_x,size_y))
+
+    target.save('result.png')
+    return result
+
+#猎蜂、阿消、空爆立绘已修正
