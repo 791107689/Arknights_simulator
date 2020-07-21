5ec9716e74e574b16c3331361f162b5cbaa870ff
SailorWontSink <xiang62917989@163.com>
2020-07-21 17:12:41 +0800

    changed all 'date' typo to 'data'

--- item.py
+++ item.py
@@ -10,18 +10,18 @@ def item_add(ID ,item_name , loc = '' , n = 10000):
 
     try:
         with open(filename) as file_object:
-            user_date = json.load(file_object)
+            user_data = json.load(file_object)
     except FileNotFoundError as e:
         return '请先注册账号\n注册方式：发送“注册 这里填用户名”'
 
     try:
-        user_date['物品'][str(item_name)] += n
+        user_data['物品'][str(item_name)] += n
     except KeyError as e:
-        user_date['物品'] = {}
-        user_date['物品'][str(item_name)] = n
+        user_data['物品'] = {}
+        user_data['物品'][str(item_name)] = n
 
     with open(filename, 'w') as file_object:
-        json.dump(user_date, file_object)
+        json.dump(user_data, file_object)
     return '成功添加' + str(item_name) + '*' + str(n)
 
 
@@ -31,20 +31,20 @@ def item_del(ID ,item_name , loc = '' , n = 10000):
 
     try:
         with open(filename) as file_object:
-            user_date = json.load(file_object)
+            user_data = json.load(file_object)
     except FileNotFoundError as e:
         return '请先注册账号\n注册方式：发送“注册 这里填用户名”'
 
     try:
-        if (user_date['物品'][str(item_name)] - n) < 0 :
+        if (user_data['物品'][str(item_name)] - n) < 0 :
             return str(item_name) + '数量不足'
         else:
-            user_date['物品'][str(item_name)] -= n
+            user_data['物品'][str(item_name)] -= n
     except KeyError as e:
         return str(item_name) + '数量不足'
 
     with open(filename, 'w') as file_object:
-        json.dump(user_date, file_object)
+        json.dump(user_data, file_object)
     return '成功删除' + str(item_name) + '*' + str(n)
 
 def item_check(ID ,loc = '' ):
@@ -52,10 +52,10 @@ def item_check(ID ,loc = '' ):
 
     try:
         with open(filename) as file_object:
-            user_date = json.load(file_object)
+            user_data = json.load(file_object)
     except FileNotFoundError as e:
         return '请先注册账号\n注册方式：发送“注册 这里填用户名”'
     try:
-        return str(user_date['物品'])
+        return str(user_data['物品'])
     except KeyError as e:
         return '无物品'
--- look_for.py
+++ look_for.py
@@ -9,6 +9,7 @@ def look_for(ID,loc = ''):
 
     inf = {}   
 
+    # 干员爆率
     six_star = 0.02
     five_star = 0.08
     four_star = 0.5
@@ -17,10 +18,10 @@ def look_for(ID,loc = ''):
     filename = loc+ '用户数据\\' + ID + '.json'
     try:
         with open(filename) as file_object:
-            user_date = json.load(file_object)
+            user_data = json.load(file_object)
     except FileNotFoundError as e:
         return '请先注册账号\n注册方式：发送“注册 这里填用户名”'
-    n = user_date['标准寻访次数']
+    n = user_data['标准寻访次数']
 
 
     real_six_star = six_star*n + six_star
@@ -29,17 +30,17 @@ def look_for(ID,loc = ''):
     real_three_star = ( (1-real_six_star) / (1-six_star) ) * three_star
 
     n += 1
-    user_date['标准寻访次数'] = n
+    user_data['标准寻访次数'] = n
 
 
     with open(filename, 'w') as file_object:
-        json.dump(user_date, file_object)
+        json.dump(user_data, file_object)
 
     p = random.random()
 
     if p <= real_six_star:
         filename = loc + r'标准寻访\标准寻访_六星.txt'
-        user_date['标准寻访次数'] = 0
+        user_data['标准寻访次数'] = 0
         inf['稀有度'] = '★★★★★★'
     elif p <= ( real_six_star + real_five_star ):
         filename = loc + r'标准寻访\标准寻访_五星.txt'
@@ -67,7 +68,7 @@ def look_for(ID,loc = ''):
 
     filename = loc+ '用户数据\\' + ID + '.json'
     with open(filename, 'w') as file_object:
-        json.dump(user_date, file_object)
+        json.dump(user_data, file_object)
     
     inf['干员'] = random.choice(card_list)
 
